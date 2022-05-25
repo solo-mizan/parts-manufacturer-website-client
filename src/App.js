4a24272d8c3dd76dad54e42e7854ad1057467ec9
solo-mizan <mizanbd.eee@gmail.com>
2022-05-25 22:34:42 +0600

    register page created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
       <Footer></Footer>
     </>
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen banner-bg-img">
-            <div class="hero-overlay bg-opacity-50"></div>
-            <div class="hero-content text-center text-neutral-content">
-                <div class="max-w-md">
-                    <h1 class="mb-5 text-7xl font-bold text-green-500">Car Makes You Crazy?</h1>
-                    <p class="mb-5 text-white text-4xl font-border">Country's Largest Car Parts Manufacturer!</p>
-                    <button class="btn btn-outline btn-xl text-white">Order Now</button>
+        <div className="hero min-h-screen banner-bg-img">
+            <div className="hero-overlay bg-opacity-50"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-7xl font-bold text-green-500">Car Makes You Crazy?</h1>
+                    <p className="mb-5 text-white text-4xl font-border">Country's Largest Car Parts Manufacturer!</p>
+                    <button className="btn btn-outline btn-xl text-white">Order Now</button>
                 </div>
             </div>
         </div>
