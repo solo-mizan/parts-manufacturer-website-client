3864a81e9dd4bed0b131852cf0a36892f267d472
solo-mizan <mizanbd.eee@gmail.com>
2022-05-25 18:01:37 +0600

    infoBanner added in homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import InfoBanner from './InfoBanner';
 import Stats from './Stats';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <InfoBanner></InfoBanner>
             <Stats></Stats>
         </div>
     );
