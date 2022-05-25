44e26db63e8ce96c37171526e550badadd824252
solo-mizan <mizanbd.eee@gmail.com>
2022-05-26 00:24:59 +0600

    authentication system added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
                         <li>
                             <a className="justify-between">
                                 Profile
-                                <span className="badge">New</span>
+                                <span className="badge">Add new</span>
                             </a>
                         </li>
                         <li><a>Settings</a></li>
